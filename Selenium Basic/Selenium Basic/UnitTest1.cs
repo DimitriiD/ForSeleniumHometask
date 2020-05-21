03606d362d5ddb7da8a59c8f1426e06a0aadaea5
DimitriiD <duchevd@yandex.ru>
2020-05-21 13:08:51 +0300

    SelniumBasicWithCanges

--- Selenium Basic/Selenium Basic/UnitTest1.cs
+++ Selenium Basic/Selenium Basic/UnitTest1.cs
@@ -30,16 +30,20 @@ namespace Selenium_Basic
             driver.Navigate().GoToUrl("http://localhost:5000/Account/Login?ReturnUrl=%2F");
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));  
+        }
+        [SetUp]
+        public void Login()
+        {
             driver.FindElement(By.XPath("//input[@id='Name']")).SendKeys("user");
             driver.FindElement(By.XPath("//input[@id='Password']")).SendKeys("user");
             driver.FindElement(By.XPath("//input[@class='btn btn-default']")).Click();
         }
         [Test,Order(1)]
-        public void Testlogandcreat()
+        public void TestLogAndCreat()
         {
-            var element = driver.FindElement(By.XPath("//h2[contains(text(),'Home page')]"));
-            Assert.IsTrue("Home page"== element.Text);
+            var hPageElement = driver.FindElement(By.XPath("//h2[contains(text(),'Home page')]"));
+            Assert.IsTrue("Home page"== hPageElement.Text);
             driver.FindElement(By.XPath("//div[2]/div[1]/a[@href='/Product']")).Click();
             driver.FindElement(By.XPath("//a[@href='/Product/Create']")).Click();
             driver.FindElement(By.XPath("//input[@id='ProductName']")).SendKeys("addfortest");
@@ -52,32 +56,32 @@ namespace Selenium_Basic
             driver.FindElement(By.XPath("//input[@id='ReorderLevel']")).SendKeys("1");
             driver.FindElement(By.XPath("//input[@id='Discontinued']")).Click();
             driver.FindElement(By.XPath("//input[@class='btn btn-default']")).Click();
-            element = driver.FindElement(By.XPath("//h2[contains(text(),'All Products')]"));
-            Assert.IsTrue("All Products" == element.Text);
+            var allPrElement = driver.FindElement(By.XPath("//h2[contains(text(),'All Products')]"));
+            Assert.IsTrue("All Products" == allPrElement.Text);
         }
         [Test, Order(2)]
         public void TestOpenProduct()
         {
             driver.FindElement(By.XPath("//div[2]/div[1]/a[@href='/Product']")).Click();
             driver.FindElement(By.XPath("//a[contains(text(),'addfortest')]")).Click();
-            var element = driver.FindElement(By.XPath("//input[@id='ProductName']"));
-            Assert.IsTrue("addfortest" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//select[@id='CategoryId']//option[@selected='selected']"));
-            Assert.IsTrue("2" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//select[@id='SupplierId']//option[@selected='selected']"));
-            Assert.IsTrue("3" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='UnitPrice']"));
-            Assert.IsTrue("300,0000" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='QuantityPerUnit']"));
-            Assert.IsTrue("35" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='UnitsInStock']"));
-            Assert.IsTrue("1" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='UnitsOnOrder']"));
-            Assert.IsTrue("1321" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='ReorderLevel']"));
-            Assert.IsTrue("1" == element.GetAttribute("value"));
-            element = driver.FindElement(By.XPath("//input[@id='Discontinued']"));
-            Assert.IsTrue("true" == element.GetAttribute("value"));
+            var pNameElement = driver.FindElement(By.XPath("//input[@id='ProductName']"));
+            Assert.IsTrue("addfortest" == pNameElement.GetAttribute("value"));
+            var catElement = driver.FindElement(By.XPath("//select[@id='CategoryId']//option[@selected='selected']"));
+            Assert.IsTrue("2" == catElement.GetAttribute("value"));
+            var supElement = driver.FindElement(By.XPath("//select[@id='SupplierId']//option[@selected='selected']"));
+            Assert.IsTrue("3" == supElement.GetAttribute("value"));
+            var uPriceElement = driver.FindElement(By.XPath("//input[@id='UnitPrice']"));
+            Assert.IsTrue("300,0000" == uPriceElement.GetAttribute("value"));
+            var qPerUnitElement = driver.FindElement(By.XPath("//input[@id='QuantityPerUnit']"));
+            Assert.IsTrue("35" == qPerUnitElement.GetAttribute("value"));
+            var uStockElement = driver.FindElement(By.XPath("//input[@id='UnitsInStock']"));
+            Assert.IsTrue("1" == uStockElement.GetAttribute("value"));
+            var uOrderElement = driver.FindElement(By.XPath("//input[@id='UnitsOnOrder']"));
+            Assert.IsTrue("1321" == uOrderElement.GetAttribute("value"));
+            var rLevelElement = driver.FindElement(By.XPath("//input[@id='ReorderLevel']"));
+            Assert.IsTrue("1" == rLevelElement.GetAttribute("value"));
+            var disconElement = driver.FindElement(By.XPath("//input[@id='Discontinued']"));
+            Assert.IsTrue("true" == disconElement.GetAttribute("value"));
         }
         [Test, Order(3)]
         public void TestDeleteLogout()
@@ -88,8 +92,8 @@ namespace Selenium_Basic
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath("//td[contains(.,'addfortest')]")));
             Assert.False(IsElementPresent(By.XPath("//td[contains(.,'addfortest')]")));
             driver.FindElement(By.XPath("//a[@href='/Account/Logout']")).Click();
-            var element = driver.FindElement(By.XPath("//h2[contains(text(),'Login')]"));
-            Assert.IsTrue("Login" == element.Text);
+            var loginElement = driver.FindElement(By.XPath("//h2[contains(text(),'Login')]"));
+            Assert.IsTrue("Login" == loginElement.Text);
         }
         [TearDown]
         public void CleanUp()
